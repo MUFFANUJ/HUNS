4c800e3eeb4f8a749820331de036770fc2186d12
AmanKumar <itsamankumar786@gmail.com>
2024-06-24 20:19:48 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/HUNS" element={<HomePage />} />
         <Route path="/contact" element={<ContactUs />} />
         
       </Routes>
