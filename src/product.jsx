44bed31cbb619e8adad3f6acc34dd8515b0921fb
AmanKumar <itsamankumar786@gmail.com>
2024-07-07 14:42:35 +0530

    update

--- src/product.jsx
+++ src/product.jsx
@@ -4,17 +4,21 @@ import { IoMdArrowRoundBack } from "react-icons/io";
 import { useNavigate } from "react-router-dom";
 
 import hoodie from "./assets/Hoodie pic 1.jpg";
-import hoodie2 from "./assets/hoodie pic white.jpg";
-import hoodie3 from "./assets/redesign blue.jpg";
-import hoodie4 from "./assets/welham brown.jpg";
-import hoodie5 from "./assets/welham turqoise.jpg";
+import hoodie2 from "./assets/welham brown.jpg";
+import hoodie3 from "./assets/welham turqoise.jpg";
+import hoodie4 from "./assets/hoddieimg.jpg";
+import hoodie5 from "./assets/hoodieimag4.jpg";
+import hoodie6 from "./assets/hoodieimg2.jpg";
+import hoodie7 from "./assets/hoodieimg3.jpg";
+import hoodie8 from "./assets/hoodie23.jpg";
 
 import tshirt from "./assets/Sd block front.jpg";
-import tshirt2 from "./assets/Sd block back.jpg";
-import tshirt3 from "./assets/tiny einstein back.jpg";
-import tshirt4 from "./assets/tiny einstein front.jpg";
-import tshirt5 from "./assets/vivo back.jpg";
-import tshirt6 from "./assets/vivo front.jpg";
+import tshirt2 from "./assets/tiny einstein front.jpg";
+import tshirt3 from "./assets/tshirtimg.jpg";
+import tshirt4 from "./assets/tshirtimg2.jpg";
+import tshirt5 from "./assets/tshirtimg3.jpg";
+import tshirt6 from "./assets/tshirtimg5.jpg";
+import tshirt7 from "./assets/vivo front.jpg";
 
 import trouser from "./assets/lady lower 1.webp";
 import trouser2 from "./assets/lady lower 3.webp";
@@ -25,68 +29,262 @@ import trouser6 from "./assets/man lower 3.webp";
 import trouser7 from "./assets/man lower 4.webp";
 import trouser8 from "./assets/man lower 5.webp";
 
-function Productpage({option,setOption}) {
+function Productpage({ option, setOption }) {
   const navigate = useNavigate();
   const [selectedCategory, setSelectedCategory] = useState("All");
-  const [selectedOption, setSelectedOption] = useState('');
-  const [showHoddie,setShowHoddie] = useState(true)
-  const [showTshirt,setShowTshir] = useState(true)
-  const [showLowers,setShowLowers] = useState(true)
+  const [selectedOption, setSelectedOption] = useState("");
+  const [showHoddie, setShowHoddie] = useState(true);
+  const [showTshirt, setShowTshir] = useState(true);
+  const [showLowers, setShowLowers] = useState(true);
 
-
-  useEffect(()=>{
+  useEffect(() => {
     window.scrollTo(0, 0);
-    if (option === 'hoddies') {
-      setShowHoddie(true)
-      setShowTshir(false)
-      setShowLowers(false)
-    } else if (option === 'tshirts') {
-      setShowHoddie(false)
-      setShowTshir(true)
-      setShowLowers(false)
-    } else if (option === 'lowers') {
-      setShowHoddie(false)
-      setShowTshir(false)
-      setShowLowers(true)
-    }else if(option === "ALL"){
-      setShowHoddie(true)
-      setShowTshir(true)
-      setShowLowers(true)
+    if (option === "hoddies") {
+      setShowHoddie(true);
+      setShowTshir(false);
+      setShowLowers(false);
+    } else if (option === "tshirts") {
+      setShowHoddie(false);
+      setShowTshir(true);
+      setShowLowers(false);
+    } else if (option === "lowers") {
+      setShowHoddie(false);
+      setShowTshir(false);
+      setShowLowers(true);
+    } else if (option === "ALL") {
+      setShowHoddie(true);
+      setShowTshir(true);
+      setShowLowers(true);
     }
-  })
+  });
 
   // Define product categories
   const products = [
-    { category: "Hoodies", items: [hoodie, hoodie2, hoodie3, hoodie4, hoodie5] },
-    { category: "T-shirts", items: [tshirt, tshirt2, tshirt3, tshirt4, tshirt5, tshirt6] },
-    { category: "Trousers", items: [trouser, trouser2, trouser3, trouser4, trouser5, trouser6, trouser7, trouser8] }
+    {
+      category: "Hoodies",
+      items: [
+        hoodie,
+        hoodie2,
+        hoodie3,
+        hoodie4,
+        hoodie5,
+        hoodie6,
+        hoodie7,
+        hoodie8,
+      ],
+    },
+    {
+      category: "T-shirts",
+      items: [tshirt, tshirt2, tshirt3, tshirt4, tshirt5, tshirt6, tshirt7],
+    },
+    {
+      category: "Trousers",
+      items: [
+        trouser,
+        trouser2,
+        trouser3,
+        trouser4,
+        trouser5,
+        trouser6,
+        trouser7,
+        trouser8,
+      ],
+    },
   ];
 
   const hoddies = [
-    {title : "anuj", imgURL:hoodie,description : "very nice hoodie wear and enjoy",sizes:"S , M , L",color:"red green blue",price:599},
-    {title : "anuj2", imgURL:hoodie2,description : "very nice hoodie wear and enjoy",sizes:"S , M , L",color:"red green blue",price:799},
-    {title : "anuj3", imgURL:hoodie3,description : "very nice hoodie wear and enjoy",sizes:"S , M , L",color:"red green blue",price:499},
-    {title : "anuj4", imgURL:hoodie4,description : "very nice hoodie wear and enjoy",sizes:"S , M , L",color:"red green blue",price:699},
-    {title : "anuj5", imgURL:hoodie5,description : "very nice hoodie wear and enjoy",sizes:"S , M , L",color:"red green blue",price : 91},
-]
+    {
+      title: "",
+      imgURL: hoodie,
+      description: "",
+      sizes: "",
+      color: "",
+      price: 0,
+    },
+    {
+      title: "",
+      imgURL: hoodie2,
+      description: "",
+      sizes: "",
+      color: "",
+      price: 0,
+    },
+    {
+      title: "",
+      imgURL: hoodie3,
+      description: "",
+      sizes: "",
+      color: "",
+      price: 0,
+    },
+    {
+      title: "",
+      imgURL: hoodie4,
+      description: "",
+      sizes: "",
+      color: "",
+      price: 0,
+    },
+    {
+      title: "",
+      imgURL: hoodie5,
+      description: "",
+      sizes: "",
+      color: "",
+      price: 0,
+    },
+    {
+      title: "",
+      imgURL: hoodie6,
+      description: "",
+      sizes: "",
+      color: "",
+      price: 0,
+    },
+    {
+      title: "",
+      imgURL: hoodie7,
+      description: "",
+      sizes: "",
+      color: "",
+      price: 0,
+    },
+    {
+      title: "",
+      imgURL: hoodie8,
+      description: "",
+      sizes: "",
+      color: "",
+      price: 0,
+    },
+  ];
+
   const tshirts = [
-    {title : "anuj", imgURL:tshirt,description : "very nice hoodie wear and enjoy",sizes:"S , M , L",color:"red green blue",price:599},
-    {title : "anuj2", imgURL:tshirt2,description : "very nice hoodie wear and enjoy",sizes:"S , M , L",color:"red green blue",price:799},
-    {title : "anuj3", imgURL:tshirt3,description : "very nice hoodie wear and enjoy",sizes:"S , M , L",color:"red green blue",price:499},
-    {title : "anuj4", imgURL:tshirt4,description : "very nice hoodie wear and enjoy",sizes:"S , M , L",color:"red green blue",price:699},
-    {title : "anuj5", imgURL:tshirt5,description : "very nice hoodie wear and enjoy",sizes:"S , M , L",color:"red green blue",price : 91},
-    {title : "anuj6", imgURL:tshirt6,description : "very nice hoodie wear and enjoy",sizes:"S , M , L",color:"red green blue",price : 91},
-]
+    {
+      title: "",
+      imgURL: tshirt,
+      description: "",
+      sizes: "",
+      color: "",
+      price: 0,
+    },
+    {
+      title: "",
+      imgURL: tshirt2,
+      description: "",
+      sizes: "",
+      color: "",
+      price: 0,
+    },
+    {
+      title: "",
+      imgURL: tshirt3,
+      description: "",
+      sizes: "",
+      color: "",
+      price: 0,
+    },
+    {
+      title: "",
+      imgURL: tshirt4,
+      description: "",
+      sizes: "",
+      color: "",
+      price: 0,
+    },
+    {
+      title: "",
+      imgURL: tshirt5,
+      description: "",
+      sizes: "",
+      color: "",
+      price: 0,
+    },
+    {
+      title: "",
+      imgURL: tshirt6,
+      description: "",
+      sizes: "",
+      color: "",
+      price: 0,
+    },
+    {
+      title: "",
+      imgURL: tshirt7,
+      description: "",
+      sizes: "",
+      color: "",
+      price: 0,
+    },
+  ];
+
   const lowers = [
-    {title : "anuj", imgURL:trouser,description : "very nice hoodie wear and enjoy",sizes:"S , M , L",color:"red green blue",price:599},
-    {title : "anuj2", imgURL:trouser2,description : "very nice hoodie wear and enjoy",sizes:"S , M , L",color:"red green blue",price:799},
-    {title : "anuj3", imgURL:trouser3,description : "very nice hoodie wear and enjoy",sizes:"S , M , L",color:"red green blue",price:499},
-    {title : "anuj4", imgURL:trouser4,description : "very nice hoodie wear and enjoy",sizes:"S , M , L",color:"red green blue",price:699},
-    {title : "anuj5", imgURL:trouser5,description : "very nice hoodie wear and enjoy",sizes:"S , M , L",color:"red green blue",price : 91},
-    {title : "anuj6", imgURL:trouser6,description : "very nice hoodie wear and enjoy",sizes:"S , M , L",color:"red green blue",price : 91},
-    {title : "anuj7", imgURL:trouser7,description : "very nice hoodie wear and enjoy",sizes:"S , M , L",color:"red green blue",price : 91},
-    {title : "anuj8", imgURL:trouser8,description : "very nice hoodie wear and enjoy",sizes:"S , M , L",color:"red green blue",price : 91},
-]
+    {
+      title: "",
+      imgURL: trouser,
+      description: "",
+      sizes: "",
+      color: "",
+      price: 0,
+    },
+    {
+      title: "",
+      imgURL: trouser2,
+      description: "",
+      sizes: "",
+      color: "",
+      price: 0,
+    },
+    {
+      title: "",
+      imgURL: trouser3,
+      description: "",
+      sizes: "",
+      color: "",
+      price: 0,
+    },
+    {
+      title: "",
+      imgURL: trouser4,
+      description: "",
+      sizes: "",
+      color: "",
+      price: 0,
+    },
+    {
+      title: "",
+      imgURL: trouser5,
+      description: "",
+      sizes: "",
+      color: "",
+      price: 0,
+    },
+    {
+      title: "",
+      imgURL: trouser6,
+      description: "",
+      sizes: "",
+      color: "",
+      price: 0,
+    },
+    {
+      title: "",
+      imgURL: trouser7,
+      description: "",
+      sizes: "",
+      color: "",
+      price: 0,
+    },
+    {
+      title: "",
+      imgURL: trouser8,
+      description: "",
+      sizes: "",
+      color: "",
+      price: 0,
+    },
+  ];
+
   // Function to handle category change
   const handleCategoryChange = (category) => {
     setSelectedCategory(category);
@@ -120,127 +318,136 @@ function Productpage({option,setOption}) {
         <IoMdArrowRoundBack size={30} />
       </div>
       <div class="radiocontainer">
-    <div class="selector">
-        {/* <div class="selector-item">
+        <div class="selector">
+          {/* <div class="selector-item">
             <input type="radio" id="radio1" name="selector" class="selector-item_radio"  value="hoddies"
             onChange={()=>handleOptionChange("ALL")}/>
             <label for="radio1" class="selector-item_label">ALL</label>
         </div> */}
-        <div class="selector-item">
-            <input type="radio" id="radio2" name="selector" class="selector-item_radio"  value="hoddies"
-            onChange={()=>{
-              setOption("hoddies")
-              }}/>
-            <label for="radio2" class="selector-item_label">Hoddies</label>
+          <div class="selector-item">
+            <input
+              type="radio"
+              id="radio2"
+              name="selector"
+              class="selector-item_radio"
+              value="hoddies"
+              onChange={() => {
+                setOption("hoddies");
+              }}
+            />
+            <label for="radio2" class="selector-item_label">
+              Hoddies
+            </label>
+          </div>
+          <div class="selector-item">
+            <input
+              type="radio"
+              id="radio3"
+              name="selector"
+              class="selector-item_radio"
+              value="tshirts"
+              onChange={() => setOption("tshirts")}
+            />
+            <label for="radio3" class="selector-item_label">
+              T-Shirts
+            </label>
+          </div>
+          <div class="selector-item">
+            <input
+              type="radio"
+              id="radio4"
+              name="selector"
+              class="selector-item_radio"
+              value="lowers"
+              onChange={() => setOption("lowers")}
+            />
+            <label for="radio4" class="selector-item_label">
+              Lowers
+            </label>
+          </div>
         </div>
-        <div class="selector-item">
-            <input type="radio" id="radio3" name="selector" class="selector-item_radio" value="tshirts" 
-            onChange={()=>setOption("tshirts")}/>
-            <label for="radio3" class="selector-item_label">T-Shirts</label>
-        </div>
-        <div class="selector-item">
-            <input type="radio" id="radio4" name="selector" class="selector-item_radio" value="lowers" 
-            onChange={()=>setOption("lowers")} />
-            <label for="radio4" class="selector-item_label">Lowers</label>
-        </div>
-    </div>
-</div>
+      </div>
       {showHoddie ? (
         <div className="products">
-        {
-          hoddies.map((item)=>{
+          {hoddies.map((item) => {
             return (
               // <div class="prodcontainer">
-      <div class="propcard" >
-        <img class="card-img" src={item.imgURL} alt="Vans" />
-        <div class="card-img-overlay d-flex justify-content-end">
+              <div class="propcard">
+                <img class="card-img" src={item.imgURL} alt="Vans" />
+                <div class="card-img-overlay d-flex justify-content-end"></div>
+                <div class="card-body">
+                  <h4 class="card-title">{item.title}</h4>
+                  <p class="card-text">{item.description}</p>
+                  <p class="card-text">Sizes: {item.sizes}</p>
+                  <p class="card-text">Color: {item.color}</p>
+                  <div class="buy d-flex justify-content-between align-items-center">
+                    <div class="price text-success">
+                      <h5 class="mt-4">{item.price}</h5>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              // </div>
+            );
+          })}
         </div>
-        <div class="card-body">
-          <h4 class="card-title">{item.title}</h4>
-          <p class="card-text">
-            {item.description}
-          </p>
-          <p class="card-text">
-            Sizes: {item.sizes}
-          </p>
-          <p class="card-text">
-            Color: {item.color}
-          </p>
-          <div class="buy d-flex justify-content-between align-items-center">
-            <div class="price text-success"><h5 class="mt-4">{item.price}</h5></div>
-          </div>
-        </div>
-      </div>
-    // </div>
-            )
-          })
-        }
-      </div>
-      ):("")}
+      ) : (
+        ""
+      )}
       {showTshirt ? (
         <div className="products">
-        {
-          tshirts.map((item)=>{
+          {tshirts.map((item) => {
             return (
               // <div class="prodcontainer">
-      <div class="propcard">
-        <img class="card-img" src={item.imgURL} alt="Vans" />
-        <div class="card-img-overlay d-flex justify-content-end">
+              <div class="propcard">
+                <img class="card-img" src={item.imgURL} alt="Vans" />
+                <div class="card-img-overlay d-flex justify-content-end"></div>
+                <div class="card-body">
+                  <h4 class="card-title">{item.title}</h4>
+                  <p class="card-text">{item.description}</p>
+                  <p class="card-text">Sizes: {item.sizes}</p>
+                  <p class="card-text">Color: {item.color}</p>
+                  <div class="buy d-flex justify-content-between align-items-center">
+                    <div class="price text-success">
+                      <h5 class="mt-4">{item.price}</h5>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              // </div>
+            );
+          })}
         </div>
-        <div class="card-body">
-          <h4 class="card-title">{item.title}</h4>
-          <p class="card-text">
-            {item.description}
-          </p>
-          <p class="card-text">
-            Sizes: {item.sizes}
-          </p>
-          <p class="card-text">
-            Color: {item.color}
-          </p>
-          <div class="buy d-flex justify-content-between align-items-center">
-            <div class="price text-success"><h5 class="mt-4">{item.price}</h5></div>
-          </div>
-        </div>
-      </div>
-    // </div>
-            )
-          })
-        }
-      </div>
-      ):("")}
+      ) : (
+        ""
+      )}
       {showLowers ? (
         <div className="products">
-        {
-          lowers.map((item)=>{
+          {lowers.map((item) => {
             return (
               // <div class="prodcontainer">
-      <div class="propcard" >
-        <img class="card-img" src={item.imgURL} alt="Vans" />
-        <div class="card-img-overlay d-flex justify-content-end">
+              <div class="propcard">
+                <img class="card-img" src={item.imgURL} alt="Vans" />
+                <div class="card-img-overlay d-flex justify-content-end"></div>
+                <div class="card-body">
+                  <h4 class="card-title">{item.title}</h4>
+                  <p class="card-text">{item.description}</p>
+                  <p class="card-text">Sizes: {item.sizes}</p>
+                  <p class="card-text">Color: {item.color}</p>
+                  <div class="buy d-flex justify-content-between align-items-center">
+                    <div class="price text-success">
+                      <h5 class="mt-4">{item.price}</h5>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              // </div>
+            );
+          })}
         </div>
-        <div class="card-body">
-          <h4 class="card-title">{item.title}</h4>
-          <p class="card-text">
-            {item.description}
-          </p>
-          <p class="card-text">
-            Sizes: {item.sizes}
-          </p>
-          <p class="card-text">
-            Color: {item.color}
-          </p>
-          <div class="buy d-flex justify-content-between align-items-center">
-            <div class="price text-success"><h5 class="mt-4">{item.price}</h5></div>
-          </div>
-        </div>
-      </div>
-    // </div>
-            )
-          })
-        }
-      </div>
-      ):("")}
+      ) : (
+        ""
+      )}
     </>
   );
 }
